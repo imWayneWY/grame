53bd18020cbc923623c8ea022b19e16bc5ad4900
Yan Wei <im.wayne.wy@gmail.com>
2021-03-28 21:17:19 -0700

    test graphql

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query {
+    getPosts {
+      username
+      body
+      createdAt
+      id
+      comments {
+        id
+        body
+        username
+      }
+    }
+  }
+`;
 
 const HomePage = () => {
-  return <div>Home Page</div>
-}
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return <div>{JSON.stringify(data.getPosts)}</div>;
+};
 
 export default HomePage;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { gql } from '@apollo/client';
+
+// const GET_USERS = gql``
 
 const LoginPage = () => {
   return <div>Login page</div>
